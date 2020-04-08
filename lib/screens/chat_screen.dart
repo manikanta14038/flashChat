256d6d0caefb518255da62f89f33d6e163db2549
manikanta <manikanta14038@gmail.com>
2020-04-08 19:42:55 +0530

    commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+   static String id='chat_screen';
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _fireStore=Firestore.instance;
+  final _auth=FirebaseAuth.instance;
+  String messageText;
+  FirebaseUser loggedInUser;
+  @override
+  void initState(){ 
+    super.initState();
+    getCurrentUser();
+    messagesStream();
+  }
+
+  void getCurrentUser() async{
+    try{
+      var user=await _auth.currentUser();
+      loggedInUser=user;
+      // print(loggedInUser);
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+  void messagesStream() async{
+    await for(var snapshot in _fireStore.collection('messages').snapshots()){
+      for(var message in snapshot.documents){
+        print(message.data);
+      }
+    }
+  }
+
+ 
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                _auth.signOut();
+                Navigator.pop(context);
+              }),
+        ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                          messageText=value;   
+                        },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      _fireStore.collection('messages').add({
+                        'msg':messageText,
+                        'user':loggedInUser.email
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
